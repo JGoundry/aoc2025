d5b4567523a094569d072438706011abc557e2c4
Josh Goundry <joshuagoundry@gmail.com>
2025-12-23 22:22:31 +0000

    Implement main.cpp for day3.

--- day3/src/main.cpp
+++ day3/src/main.cpp
@@ -1,5 +1,34 @@
+#include <iostream>
+#include <print>
+
 #include "maxJoltage.hpp"
+#include "utils/fileUtils.hpp"
 
 int main(int argc, char** argv) {
-   return day3::maxJoltage({});
+  if (argc != 2) {
+    std::println(std::cerr, "Usage: {} <input_file>", argv[0]);
+    return -1;
+  }
+
+  // Validate filepath and open file
+  const std::string_view filename = argv[1];
+  auto file = utils::openFile(filename, std::ios_base::in);
+  if (!file) {
+    std::cerr << file.error() << '\n';
+    return -1;
+  }
+
+  // Read lines from file
+  const auto batteryBanks = utils::readLines(*file);
+  if (!batteryBanks) {
+    std::cerr << batteryBanks.error() << '\n';
+    return -1;
+  }
+
+  // Get max jolts
+  const auto maxJoltage = day3::maxJoltage(*batteryBanks);
+
+  std::println("Max joltage: {}", maxJoltage);
+
+  return 0;
 }
