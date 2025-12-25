f3714bedba476c430a63931951fb8db8c3d5dbd4
Josh Goundry <joshuagoundry@gmail.com>
2025-12-25 18:40:14 +0000

    Implement day 5 part 1.

--- day5/src/totalFreshIngredients.cpp
+++ day5/src/totalFreshIngredients.cpp
@@ -1,10 +1,52 @@
 #include "totalFreshIngredients.hpp"
 
+#include <algorithm>
+#include <exception>
+#include <expected>
+#include <iostream>
+#include <string>
+
+#include "utils/stringUtils.hpp"
+
 namespace day5 {
 
-std::uint64_t totalFreshIngredients(const std::vector<std::string>& database)
-{
-    return {};
+std::uint64_t totalFreshIngredients(const std::vector<std::string>& database) {
+  std::vector<utils::Range> freshRanges;
+
+  std::uint64_t totalFresh{};
+
+  bool processingAvaliableIngredients{};
+  for (const auto& entry : database) {
+    if (entry.empty()) {
+      processingAvaliableIngredients = true;
+      continue;
+    }
+
+    // Process range
+    if (!processingAvaliableIngredients) {
+      const auto range = utils::parseRange(entry);
+      if (!range) {
+        std::println(std::cerr, "{}", range.error());
+        continue;
+      }
+      freshRanges.emplace_back(*range);
+      continue;
+    }
+
+    // Process ingredient
+    try {
+      auto ingredientID = std::stoll(entry);
+
+      totalFresh += std::ranges::any_of(freshRanges, [ingredientID](const auto& freshRange) {
+        return ingredientID >= freshRange.lower &&
+               ingredientID <= freshRange.upper;
+      });
+    } catch (...) {
+      std::println(std::cerr, "Invalid ID: {}", entry);
+    }
+  }
+
+  return totalFresh;
 }
 
-}
+}  // namespace day5
